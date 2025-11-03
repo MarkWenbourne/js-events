b2876a8ec9e0d6b57baea6a731fa71081808675c
MarkWenbourne <markwenbourne1@gmail.com>
2025-11-03 14:54:03 -0800

    Events Pt 2

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-window.addEventListener("click", function() {
+window.addEventListener("click", function(event) {
     console.log("a click occurred on the window")
+    console.log("event.Target:", event.Target)
+    console.log("event.currentTarget:", event.currentTarget)
 })
 
 function handleButtonClick(event) {
     console.log("a button was clicked, event:", event)
+    console.log("event.Target:", event.Target)
+    console.log("event.currentTarget:", event.currentTarget)
     var button = event.target
     var box = button.parentNode
     box.classList.toggle("highlighted")
@@ -13,3 +17,17 @@ var buttons = document.getElementsByClassName("in-box-button")
 for (var i = 0; i <buttons.length; i++) {
     buttons[i].addEventListener("click", handleButtonClick)
 }
+
+var boxContainer = document.getElementById("box-container")
+boxContainer.addEventListener("click", function (event) {
+    console.log("the main element was click")
+    console.log("event.Target:", event.Target)
+    console.log("event.currentTarget:", event.currentTarget)
+    event.stopPropagation()
+})
+
+var link = document.getElementById("website-link")
+link.addEventListener("click", function (event) {
+    console.log("that link was click")
+    event.preventDefault()
+})
